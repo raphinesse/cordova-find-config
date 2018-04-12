6385f079bdd55132cbe43537e3c0cf6fd3e359f5
Raphael von der Grün <raphinesse@gmail.com>
2018-04-12 10:24:16 +0200

    Add tests for config present in two locations

--- test.js
+++ test.js
@@ -5,10 +5,12 @@ test('async', async t => {
   t.is(await m('.'), undefined)
   t.is(await m('fixture/in-root'), 'fixture/in-root/config.xml')
   t.is(await m('fixture/in-www'), 'fixture/in-www/www/config.xml')
+  t.is(await m('fixture/double-trouble'), 'fixture/double-trouble/config.xml')
 })
 
 test('sync', t => {
   t.is(m.sync('.'), undefined)
   t.is(m.sync('fixture/in-root'), 'fixture/in-root/config.xml')
   t.is(m.sync('fixture/in-www'), 'fixture/in-www/www/config.xml')
+  t.is(m.sync('fixture/double-trouble'), 'fixture/double-trouble/config.xml')
 })
